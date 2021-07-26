1780f3e0f3a99f8c8920f676471ed6e4b1dd9702
SexyManz <84806867+SexyManz@users.noreply.github.com>
2021-07-27 00:08:19 +0430

    Add files via upload

---
+++ SEXLEAK_hud2/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+author"§êx¥│måñ#2193"
+
+game 'gta5'
+
+description 'sexleak'
+
+
+ui_page 'html/ui.html'
+files {
+	'html/style.css',
+	'html/style.js',
+	'html/burger.png',
+	'html/heart.png',
+	'html/oxygen.png',
+	'html/sheild.png',
+	'html/tension.png',
+	'html/water.png',
+	'html/ui.html'
+}
+
+
+client_scripts {'cl.lua'}
