4f0c4153e25727f491ed30bfba6878e4b167e22f
SexyManz <84806867+SexyManz@users.noreply.github.com>
2021-07-27 00:13:36 +0430

    Add files via upload

---
+++ SEXLEAK_hud/__resource.lua
@@ -0,0 +1,27 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+author"§êx¥│måñ#2193"
+ui_page "ui/index.html"
+files {
+	"ui/index.html",
+	"ui/imgs/test.png",
+	"ui/fonts/fonts/Circular-Bold.ttf",
+	"ui/fonts/fonts/Circular-Bold.ttf",
+	"ui/fonts/fonts/Circular-Regular.ttf",
+	"ui/script.js",
+	"ui/style.css",
+	"ui/debounce.min.js",
+		-- Job Images
+	'ui/assets/imgs/*.png',
+	
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
+
+
+ 
---
+++ SEXLEAK_hud/readme.md
@@ -0,0 +1,9 @@
+# Joker City Hud
+This Hud Publiced By [Sex Leak](https://discord.gg/xY8JngNJtc)
+<br>
+
+If You Have Issue Conect Us In [Sex Leak](https://discord.gg/xY8JngNJtc) 
+
+# IMGS
+<img src="https://cdn.discordapp.com/attachments/856559759947595787/869291090786779166/Screenshot_641.png"></img>
+<img src="https://cdn.discordapp.com/attachments/856559759947595787/869291257271287869/Screenshot_642.png"></img>
